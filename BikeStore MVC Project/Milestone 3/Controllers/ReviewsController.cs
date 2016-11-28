589ba116f2cea09b308d38858c2733ec89164bcc
milanopatela <darkrai60@gmail.com>
2016-11-28 10:30:45 -0500

    Added Review
    Users can now add a review
    TODO: Show reviews made of a product on the productdetails page

---
+++ BikeStore MVC Project/Milestone 3/Controllers/ReviewsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MileStone2A.Models;
+using Milestone_3.Models;
+
+namespace Milestone_3.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private BikeDBContext db = new BikeDBContext();
+
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            return View(db.Reviews.ToList());
+        }
+
+        public ActionResult AddReview(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            Reviews review = new Reviews();
+            review.ProductID = product.ProductID;
+            return View(review);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddReview([Bind(Include = "id,Name,ProductID,Review,Rating")] Reviews reviews)
+        {
+            if (ModelState.IsValid)
+            {
+                if (reviews.Name == null || reviews.Name == "" || reviews.Name == " ")
+                {
+                    reviews.Name = "Anonymous";
+                }
+                db.Reviews.Add(reviews);
+                db.SaveChanges();
+                return RedirectToAction("ReviewSuccess");
+            }
+
+            return View(reviews);
+        }
+
+        public ActionResult ReviewSuccess()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
