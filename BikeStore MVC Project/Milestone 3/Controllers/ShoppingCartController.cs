5b2e028755ad10d43694cdeaa7195056cec37ee4
justinlefebvre <1354629@johnabbottcollege.net>
2016-11-29 11:02:34 -0500

    somewhat cart

---
+++ BikeStore MVC Project/Milestone 3/Controllers/ShoppingCartController.cs
@@ -0,0 +1,80 @@
+﻿using Milestone_3.ViewModels;
+using MileStone2A.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Milestone_3.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        BikeStoreEntities storeDB = new BikeStoreEntities();
+        private BikeDBContext db = new BikeDBContext();
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Set up our ViewModel
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+            // Return the view
+            return View(viewModel);
+        }
+        public ActionResult AddToCart(int id)
+        {
+            var item = (from x in db.Products where x.ProductID == id select x).First();
+            // Retrieve the album from the database
+
+            // Add it to the shopping cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(item);
+
+            // Go back to the main store page for more shopping
+            return RedirectToAction("Index");
+        }
+        //
+        // AJAX: /ShoppingCart/RemoveFromCart/5
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            // Remove the item from the cart
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            // Get the name of the album to display confirmation
+            string productName = storeDB.Carts
+                .Single(item => item.ProductId == id).Product.Name;
+
+            // Remove from cart
+            int itemCount = cart.RemoveFromCart(id);
+
+            // Display the confirmation message
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(productName) +
+                    " has been removed from your shopping cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+            return Json(results);
+        }
+        //
+        // GET: /ShoppingCart/CartSummary
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+    }
+}
---
+++ BikeStore MVC Project/Milestone 3/Models/BikeStoreEntities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MileStone2A.Models
+{
+    public class BikeStoreEntities : DbContext
+    {
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductCategory> ProductCategory { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
